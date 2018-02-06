af847e5c8e7b9e4bea32e9bad2368a417c7f7d65
themilanguy1 <milan.gupta@ymail.com>
2018-02-06 09:12:42 +0100

    w

--- opdracht-1.php
+++ opdracht-1.php
@@ -1,5 +1,14 @@
 <?php
+session_start();
 
+$_SESSION["bekeken"] = $_SESSION["bekeken"] + 1;
+
+if(isset ($_SESSION["bekeken"])) {
+    echo "pagina bezocht: ".$_SESSION["bekeken"]. " keer";
+}
+else {
+    echo "sessie nog niet geset";
+}
 
 
 
